74b8ceda73035876fd9ae342123c6d7f2e40d039
YEREMIA CHRIS SARAGI <yeremia997@gmail.com>
2025-06-16 17:20:39 +0700

    feat: update read minutes

--- app/notes/components/Blog.tsx
+++ app/notes/components/Blog.tsx
@@ -18,7 +18,26 @@ const defaultMotionProps = {
   transition: { duration: 0.8 },
 };
 
+interface NoteCardProps {
+  href: string;
+  badge: string;
+  title: string;
+  description: string;
+  date: string;
+  readTime: string;
+}
+
 export const Blog = () => {
+  const noteCardContent = {
+    href: "/notes/1",
+    badge: "#react",
+    title: "TanStack Query: Simplifying Data Fetching in React",
+    description:
+      "Explore how TanStack Query, formerly known as React Query, simplifies data fetching, caching, and synchronization in React applications. Learn to use the useQuery hook for efficient data management.",
+    date: "Sep 02, 2021",
+    readTime: "3 min read",
+  };
+
   return (
     <motion.section
       {...defaultMotionProps}
@@ -27,37 +46,39 @@ export const Blog = () => {
     >
       <Title title="Notes" description="My study references" />
       <div className="grid gap-5">
-        <NoteCard />
+        <NoteCard {...noteCardContent} />
       </div>
     </motion.section>
   );
 };
 
-const NoteCard = () => {
+const NoteCard = ({
+  href,
+  badge,
+  title,
+  description,
+  date,
+  readTime,
+}: NoteCardProps) => {
   return (
-    <Link href={`/notes/1`}>
+    <Link href={href}>
       <Card className="bg-transparent text-white border-0 rounded-none pb-0">
         <CardContent className="p-0">
           <div className="flex flex-col md:flex-row items-center justify-between gap-5">
             <div className="space-y-3">
-              <Badge>#react</Badge>
-              <CardTitle className="text-xl">
-                TanStack Query: Simplifying Data Fetching in React
-              </CardTitle>
+              <Badge>{badge}</Badge>
+              <CardTitle className="text-xl">{title}</CardTitle>
               <CardDescription className="text-neutral-200">
-                Explore how TanStack Query, formerly known as React Query,
-                simplifies data fetching, caching, and synchronization in React
-                applications. Learn to use the useQuery hook for efficient data
-                management.
+                {description}
               </CardDescription>
               <div className="flex gap-5">
                 <div className="flex items-center text-neutral-400 gap-1">
                   <Calendar width={12} />
-                  <span className="text-xs"> Sep 02, 2021 </span>
+                  <span className="text-xs">{date}</span>
                 </div>
                 <div className="flex items-center text-neutral-400 gap-1">
                   <Timer width={14} />
-                  <span className="text-xs"> 10 min read </span>
+                  <span className="text-xs">{readTime}</span>
                 </div>
               </div>
             </div>

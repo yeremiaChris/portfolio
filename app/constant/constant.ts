672bc24c4c8dbfc3a7b703ff46ae7f0f700bf662
YEREMIA CHRIS SARAGI <yeremia997@gmail.com>
2025-10-30 11:04:35 +0700

    feat: update cv link

--- app/constant/constant.ts
+++ app/constant/constant.ts
@@ -3,4 +3,4 @@ export const LINKEDIN_URL =
   "https://www.linkedin.com/in/yeremia-chris-saragi-587a671a9/";
 export const EMAIL_URL = "mailto:yeremia997@gmail.com";
 export const RESUME_URL =
-  "https://drive.google.com/file/d/1OVi_8YF4i_cFV-wRsVgwdIINdPzUdDsO/view?usp=sharing";
+  "https://docs.google.com/document/d/1PFzw-wlXXIgONKRkJ2YQd1k50uR1P7aIsAr-qPqU4qE/edit?usp=sharing";
